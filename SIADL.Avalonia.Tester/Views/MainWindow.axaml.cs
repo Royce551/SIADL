76078a2b55d5c7b35ea248b5647dbde029332dd8
Royce551 <poohwie2005@gmail.com>
2022-01-27 20:39:50 -0600

    working dynamic theme switching i think?

--- SIADL.Avalonia.Tester/Views/MainWindow.axaml.cs
+++ SIADL.Avalonia.Tester/Views/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
+using Avalonia.Themes.Fluent;
 
 namespace SIADL.Avalonia.Tester.Views
 {
@@ -12,6 +14,41 @@ namespace SIADL.Avalonia.Tester.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
+            var themes = this.Find<ComboBox>("Themes");
+            themes.SelectionChanged += (sender, e) =>
+            {
+                switch (themes.SelectedIndex)
+                {
+                    case 0:
+                        Styles[0] = new StyleInclude(new System.Uri("avares://SIADL.Avalonia"))
+                        {
+                            Source = new System.Uri("avares://SIADL.Avalonia/LightTheme.axaml")
+                        };
+                        Styles[1] = new StyleInclude(new System.Uri("avares://SIADL.Avalonia"))
+                        {
+                            Source = new System.Uri("resm:Avalonia.Themes.Default.Accents.BaseLight.xaml?assembly=Avalonia.Themes.Default")
+                        };
+                        break;
+                    case 1:
+                        Styles[0] = new StyleInclude(new System.Uri("avares://SIADL.Avalonia"))
+                        {
+                            Source = new System.Uri("avares://SIADL.Avalonia/DarkTheme.axaml")
+                        };
+                        Styles[1] = new StyleInclude(new System.Uri("avares://SIADL.Avalonia"))
+                        {
+                            Source = new System.Uri("resm:Avalonia.Themes.Default.Accents.BaseDark.xaml?assembly=Avalonia.Themes.Default")
+                        };
+                        break;
+                }
+            };
+            Styles.Add(new StyleInclude(new System.Uri("avares://SIADL.Avalonia"))
+            {
+                Source = new System.Uri("avares://SIADL.Avalonia/LightTheme.axaml")
+            });
+            Styles.Add(new StyleInclude(new System.Uri("avares://SIADL.Avalonia"))
+            {
+                Source = new System.Uri("resm:Avalonia.Themes.Default.Accents.BaseLight.xaml?assembly=Avalonia.Themes.Default")
+            });
         }
 
         private void InitializeComponent()
